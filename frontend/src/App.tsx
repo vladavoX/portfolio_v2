3a17abc25e43e85afc446faeceb69be7585d6be6
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-11-08 09:40:54 +0100

    Setup react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Sidebar from './components/Sidebar';
+
 import './App.css';
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+      <Outlet />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+    ]
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
